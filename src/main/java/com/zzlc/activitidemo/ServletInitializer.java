d7974636dcbcc1ee1af9dd4d6cf1896904390f63
luwg <luwg@zzlc.com>
2018-11-26 17:44:09 +0800

    init

---
+++ src/main/java/com/zzlc/activitidemo/ServletInitializer.java
@@ -0,0 +1,26 @@
+package com.zzlc.activitidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@ServletComponentScan
+//@EnableTransactionManagement
+//@MapperScan(basePackages = "com.river.dao") //整合mybatis 扫描路径
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServletInitializer.class);
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletInitializer.class, args);
+    }
+}
